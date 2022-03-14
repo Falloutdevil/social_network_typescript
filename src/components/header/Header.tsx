2fac5048af22a8f7aff80d9e48e3421ba4e8e277
Falloutdevil <Falloutdevil@ya.ru>
2022-03-14 21:16:26 +0300

    add components (12)

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export const Header = () => {
+  return (
+      <header className="header">
+          <img className="logo"
+               src="https://toppng.com/uploads/preview/coffee-logo-png-file-1152606554110umdspn7i.png"
+               alt="logo"/>
+      </header>
+  )
+}
---
+++ src/components/profile/Profile.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+export const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img className="coverImg"
+                     src="https://images.unsplash.com/photo-1446769357257-5aa1b1bfcd65?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                     alt=""/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                my posts
+            </div>
+            <div>post 1</div>
+            <div>post 2</div>
+        </div>
+    )
+}
