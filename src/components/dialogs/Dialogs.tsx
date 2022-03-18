e7ba892a86f7740075b6a791732db0972426fc74
Falloutdevil <Falloutdevil@ya.ru>
2022-03-18 19:17:11 +0300

    add Dialogs.module.css (21)

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -5,8 +5,23 @@ import s from './Dialogs.module.css';
 
 export const Dialogs = () => {
   return (
-      <div>
-        1
+      <div className={s.dialogs}>
+        <div className={s.dialogsItems}>
+            <div className={s.dialog + ' ' + s.active}>
+                    Nick
+            </div>
+            <div className={s.dialog}>
+                    Pick
+            </div>
+            <div className={s.dialog}>
+                    Vika
+            </div>
+        </div>
+          <div className={s.messages}>
+              <div className={s.message}>Hi</div>
+              <div className={s.message}>123123</div>
+              <div className={s.message}>sdfsdf</div>
+          </div>
       </div>
   )
 }
